8f0a5ddecbc10f4efacadd15cc061fd6941d4b6b
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-01-31 22:32:02 +0530

    `Added import of Text component and health data from StudentsDb, and updated Health component to display student health records.`

--- my-app/components/Health.js
+++ my-app/components/Health.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import { ScrollView, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import { healthTracks } from './StudentsDb';
 
-export default function Health({route}) {
+export default function Health({ route }) {
   const { student } = route.params;
+  const health = healthTracks.find(health => health.student_id === student.id)
 
   return (
     <ScrollView>
       <View>
         <Text>{student.name}</Text>
+        <Text>Last Checkup Date: {health.last_checkup}</Text>
+        <Text>Health Records</Text>
+        <Text>Height: {health.height}</Text>
+        <Text>Weight: {health.weight}</Text>
+        <Text>Heart Rate: {health.heart_rate}</Text>
+        <Text>Blood Pressure: {health.blood_pressure}</Text>
+        <Text>Exercise Frequency: {health.exercise_frequency}</Text>
+        <Text>Dietary Preference: {health.dietary_preference}</Text>
+        <Text>Medical Condition: {health.medical_conditions}</Text>
       </View>
     </ScrollView>
   )
